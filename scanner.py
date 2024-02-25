872341a87238fd20f4c26818db16979761ad1013
markwilland <markwillanderson@gmail.com>
2024-02-24 20:08:40 -0600

    replaced http request with websocket

--- scanner.py
+++ scanner.py
@@ -1,12 +1,41 @@
-from dotenv import load_dotenv
 import os
+import time
 
 from web3 import Web3
 
 API_KEY = os.getenv('INFURA_API_KEY')
 
-url = f"https://mainnet.infura.io/v3/{API_KEY}"
+infura_ws = f"wss://mainnet.infura.io/ws/v3/{API_KEY}"
 
-w3 = Web3(Web3.HTTPProvider(url))
-res = w3.is_connected()
-print(res)
+w3 = Web3(Web3.WebsocketProvider(infura_ws))
+print("Connection successful: ", w3.is_connected)
+
+def handle_new_block(block_hash):
+    print("----------- HANDLE EVENT ----------------")
+    block = w3.eth.get_block(block_hash.hex(), full_transactions=True)
+    
+    print("-------------------- BLOCK DETAIL ---------------")
+    print(f"New Block Number: {block.number}")
+    print(f"Hash: {block.hash.hex()}")
+    print(f"Parent Hash: {block.parentHash.hex()}")
+    print(f"Miner: {block.miner}")
+    print(f"Transactions: {len(block.transactions)}")
+
+    print("---------------- TX Details ----------------")
+    print(block.transactions) 
+
+    
+    # for tx in block.transactions:
+
+    # if tx.to is None:
+
+
+def log_loop(event_filter, poll_interval):
+    while True:
+        for event in event_filter.get_new_entries():
+            handle_new_block(event)
+        time.sleep(poll_interval)
+
+new_block_filter = w3.eth.filter('latest')
+
+log_loop(new_block_filter, 60)
