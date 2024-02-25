2401a1b5ead9e124b3274647add3f4cc4b5225a7
markwilland <markwillanderson@gmail.com>
2024-02-24 18:31:20 -0600

    establish basic connection

--- scanner.py
+++ scanner.py
@@ -3,5 +3,10 @@ import os
 
 from web3 import Web3
 
-infura_api_key = os.getenv('INFURA_API_KEY')
+API_KEY = os.getenv('INFURA_API_KEY')
 
+url = f"https://mainnet.infura.io/v3/{API_KEY}"
+
+w3 = Web3(Web3.HTTPProvider(url))
+res = w3.is_connected()
+print(res)
